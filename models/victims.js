73fc6f760cbdf43e9ad1dbda5abd0c03e7b90611
Dani G <daniellegarcia@MacBook-Air-3.attlocal.net>
2018-08-30 09:40:32 -0400

    .

---
+++ models/victims.js
@@ -0,0 +1,56 @@
+module.exports = function(sequelize, DataTypes) {
+  var Victim = sequelize.define("Victim", {
+    first_name:   {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING, 
+      allowNull: false
+    },
+    gender: {
+      type: DataTypes.STRING, 
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    testimonial: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    media_resources: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true
+      }
+    },
+    contact_person_name: {
+      type: DataTypes.STRING
+    },
+    contact_person_email: {
+      type: DataTypes.STRING
+    },
+    reported_by: {
+      type: DataTypes.STRING
+    }
+  },
+    {
+    timestamps: false
+  });
+
+  return Victim;
+
+};
+
+
+
+
+
+
